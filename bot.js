a2341fee149b08e18a02f066c3b1191a3d246673
ezedin ebrahim <ezedinebrahim131@gmail.com>
2024-12-27 12:13:25 +0300

    deploying89 ...

--- bot.js
+++ bot.js
@@ -15,6 +15,7 @@ const bot = new TelegramBot(token); // No polling
 const librarianChatId = process.env.LIBRARIAN_CHAT_ID.trim(); // Set this to the logged chat ID
 async function connectToDatabase() {
   try {
+    console.log("MongoDB URI:", process.env.MONGO_URI); // Log the URI
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
