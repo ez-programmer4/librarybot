c77f9cdb3dd794dfbf15b42892a3de4cc81e8464
ezedin ebrahim <ezedinebrahim131@gmail.com>
2025-01-10 23:37:44 +0300

    deplying 8

--- bot.js
+++ bot.js
@@ -803,7 +803,7 @@ bot.onText(/\/my_reservations/, async (msg) => {
     })
     .join("\n");
 
-  const message = `✨ Your Reservations: ✨\n\n${reservationList}\n\n ❌ To cancel a reservation, \n type ${`/cancel__reservation`} <book_id>.`;
+  const message = `✨ Your Reservations: ✨\n\n${reservationList}\n\n ❌ To cancel a reservation, \n type ${`/cancel_reservation`} <book_id>.`;
 
   // Send message in chunks if necessary
   await sendMessageInChunks(chatId, message);
